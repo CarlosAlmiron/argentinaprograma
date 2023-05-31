590bd1011e61686e2974256f4c8bccf52eb5bf44
Carlos Almirón <CarlosAlmiron>
2023-05-31 00:36:38 -0300

    agrego el archivo conversionGrados.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,9 @@
+// a.
+let gradosFahrenheit = 50;
+let fahrenheitACelsius = (gradosFahrenheit - 32) * 5/9;
+console.log(fahrenheitACelsius);
+
+// b.
+gradosFahrenheit = 75;
+fahrenheitACelsius = (gradosFahrenheit - 32) * 5/9;
+console.log(fahrenheitACelsius);
