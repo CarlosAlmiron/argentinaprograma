c63eb79461f13266ffa647939141aa54dc875a00
Carlos Almirón <CarlosAlmiron>
2023-05-31 00:39:43 -0300

    Practico N0 finalizado

---
+++ practico0/promedio3.js
@@ -0,0 +1,20 @@
+// a.
+let numero1 = 5;
+let numero2 = 7;
+let numero3 = 9;
+let promedio = (numero1 + numero2 + numero3) / 3;
+console.log(promedio);
+
+// b.
+numero1 = -2;
+numero2 = -3;
+numero3 = -8;
+promedio = (numero1 + numero2 + numero3) / 3;
+console.log(promedio);
+
+// c.
+numero1 = -1.2;
+numero2 = 5.3;
+numero3 = -3.3;
+promedio = (numero1 + numero2 + numero3) / 3;
+console.log(promedio);
